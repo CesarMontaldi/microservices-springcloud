183ead112ecafcdde987d4d8702f8017cec388a6
guto <guto_montaldi@yahoo.com.br>
2024-12-18 10:03:31 -0300

    Criacao do microservice Avaliador de Credito e registro no Eureka

---
+++ msavalidorcredito/src/main/java/br/com/cesarmontaldi/msavalidorcredito/MsavalidorcreditoApplication.java
@@ -0,0 +1,15 @@
+package br.com.cesarmontaldi.msavalidorcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsavalidorcreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsavalidorcreditoApplication.class, args);
+	}
+
+}
---
+++ msavalidorcredito/src/test/java/br/com/cesarmontaldi/msavalidorcredito/MsavalidorcreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesarmontaldi.msavalidorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavalidorcreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
